81bf7fb41410e8ac5744016bd9a54620bb19ecd8
Александр Богомаз <bogomazaa0411@gmail.com>
2025-02-26 14:11:43 +0500

    Добавьте файлы проекта.

---
+++ SeparationSecret/Form1.cs
@@ -0,0 +1,10 @@
+namespace SeparationSecret
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SeparationSecret/SeparationSecret.cs
@@ -0,0 +1,15 @@
+namespace SeparationSecret
+{
+    public partial class SeparationSecret : Form
+    {
+        public SeparationSecret()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
