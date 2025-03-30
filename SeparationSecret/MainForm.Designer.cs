e2fd010b7fd97375c12691c5189cc278703a4800
Александр Богомаз <bogomazaa0411@gmail.com>
2025-03-31 00:11:10 +0500

    добавил визуал

--- SeparationSecret/MainForm.Designer.cs
+++ SeparationSecret/MainForm.Designer.cs
@@ -38,6 +38,22 @@ namespace SeparationSecret
             языкToolStripMenuItem = new ToolStripMenuItem();
             русскийToolStripMenuItem = new ToolStripMenuItem();
             английскийToolStripMenuItem = new ToolStripMenuItem();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label7 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            textBox2 = new TextBox();
+            textBox8 = new TextBox();
+            textBox1 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
             menuStrip1.SuspendLayout();
             SuspendLayout();
             // 
@@ -46,7 +62,7 @@ namespace SeparationSecret
             menuStrip1.Items.AddRange(new ToolStripItem[] { файлToolStripMenuItem, справкаToolStripMenuItem, языкToolStripMenuItem });
             menuStrip1.Location = new Point(0, 0);
             menuStrip1.Name = "menuStrip1";
-            menuStrip1.Size = new Size(700, 24);
+            menuStrip1.Size = new Size(828, 24);
             menuStrip1.TabIndex = 0;
             menuStrip1.Text = "menuStrip1";
             // 
@@ -83,7 +99,7 @@ namespace SeparationSecret
             руководствоПользователяToolStripMenuItem.Name = "руководствоПользователяToolStripMenuItem";
             руководствоПользователяToolStripMenuItem.Size = new Size(221, 22);
             руководствоПользователяToolStripMenuItem.Text = "Руководство пользователя";
-            руководствоПользователяToolStripMenuItem.Click += this.руководствоПользователяToolStripMenuItem_Click;
+            руководствоПользователяToolStripMenuItem.Click += руководствоПользователяToolStripMenuItem_Click;
             // 
             // языкToolStripMenuItem
             // 
@@ -104,17 +120,185 @@ namespace SeparationSecret
             английскийToolStripMenuItem.Size = new Size(119, 22);
             английскийToolStripMenuItem.Text = "English";
             // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(83, 92);
+            label2.Name = "label2";
+            label2.Size = new Size(95, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Введите секрет :";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(83, 121);
+            label3.Name = "label3";
+            label3.Size = new Size(208, 15);
+            label3.TabIndex = 3;
+            label3.Text = "Введите количество частей секрета :";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(83, 154);
+            label4.Name = "label4";
+            label4.Size = new Size(448, 15);
+            label4.TabIndex = 4;
+            label4.Text = "Введите минимально необходимое кол-во частей секрета для восстановления :";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label5.Location = new Point(48, 181);
+            label5.Name = "label5";
+            label5.Size = new Size(94, 21);
+            label5.TabIndex = 5;
+            label5.Text = "Результат :";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(273, 334);
+            label7.Name = "label7";
+            label7.Size = new Size(126, 15);
+            label7.TabIndex = 7;
+            label7.Text = "Введите n-ый секрет :";
+            label7.Click += label7_Click;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(273, 295);
+            label10.Name = "label10";
+            label10.Size = new Size(208, 15);
+            label10.TabIndex = 10;
+            label10.Text = "Введите количество частей секрета :";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label11.Location = new Point(239, 366);
+            label11.Name = "label11";
+            label11.Size = new Size(94, 21);
+            label11.TabIndex = 11;
+            label11.Text = "Результат :";
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.Menu;
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            button1.Location = new Point(48, 47);
+            button1.Name = "button1";
+            button1.Size = new Size(157, 36);
+            button1.TabIndex = 12;
+            button1.Text = "Разделить секрет";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.Menu;
+            button2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            button2.Location = new Point(239, 259);
+            button2.Name = "button2";
+            button2.Size = new Size(181, 33);
+            button2.TabIndex = 13;
+            button2.Text = "Восстановить секрет";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.Menu;
+            textBox2.Location = new Point(184, 89);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 15;
+            // 
+            // textBox8
+            // 
+            textBox8.BackColor = SystemColors.Menu;
+            textBox8.Location = new Point(297, 118);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(100, 23);
+            textBox8.TabIndex = 21;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.Menu;
+            textBox1.Location = new Point(537, 151);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 22;
+            // 
+            // textBox4
+            // 
+            textBox4.BackColor = SystemColors.Menu;
+            textBox4.Location = new Point(339, 368);
+            textBox4.Name = "textBox4";
+            textBox4.ReadOnly = true;
+            textBox4.Size = new Size(100, 23);
+            textBox4.TabIndex = 24;
+            // 
+            // textBox5
+            // 
+            textBox5.BackColor = SystemColors.Menu;
+            textBox5.Location = new Point(405, 331);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(100, 23);
+            textBox5.TabIndex = 25;
+            // 
+            // textBox6
+            // 
+            textBox6.BackColor = SystemColors.Menu;
+            textBox6.Location = new Point(487, 292);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(100, 23);
+            textBox6.TabIndex = 26;
+            // 
+            // textBox7
+            // 
+            textBox7.BackColor = SystemColors.Menu;
+            textBox7.Location = new Point(148, 183);
+            textBox7.Name = "textBox7";
+            textBox7.ReadOnly = true;
+            textBox7.Size = new Size(100, 23);
+            textBox7.TabIndex = 27;
+            textBox7.TextChanged += textBox7_TextChanged;
+            // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(700, 338);
+            BackColor = SystemColors.Window;
+            ClientSize = new Size(828, 458);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox1);
+            Controls.Add(textBox8);
+            Controls.Add(textBox2);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label7);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
             Controls.Add(menuStrip1);
             MainMenuStrip = menuStrip1;
             Margin = new Padding(3, 2, 3, 2);
+            MaximizeBox = false;
+            MinimizeBox = false;
             Name = "MainForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Separation Secret";
+            Load += MainForm_Load;
             menuStrip1.ResumeLayout(false);
             menuStrip1.PerformLayout();
             ResumeLayout(false);
@@ -138,5 +322,21 @@ namespace SeparationSecret
         private ToolStripMenuItem языкToolStripMenuItem;
         private ToolStripMenuItem русскийToolStripMenuItem;
         private ToolStripMenuItem английскийToolStripMenuItem;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label7;
+        private Label label10;
+        private Label label11;
+        private Button button1;
+        private Button button2;
+        private TextBox textBox2;
+        private TextBox textBox8;
+        private TextBox textBox1;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
     }
 }
--- SeparationSecret/MainForm.cs
+++ SeparationSecret/MainForm.cs
@@ -37,5 +37,40 @@ namespace SeparationSecret
             about_the_program about_the_program = new about_the_program();
             about_the_program.ShowDialog();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
